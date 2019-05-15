e441b59c524bd8832a37dea4385d564d20b7a0ee
sunabove <sunabove@nate.com>
2019-05-15 00:13:04 -0700

    **

---
+++ ubuntu/ros_run.py
@@ -0,0 +1,44 @@
+#coding: utf-8
+
+import subprocess
+
+print( "Hello" )
+
+#proc    -> name/id of the process
+#id = 1  -> search for pid
+#id = 0  -> search for name (default)
+
+def process_exists( proc ):
+    ps = subprocess.Popen("ps -aux", shell=True, stdout=subprocess.PIPE) 
+    output = ps.stdout.read()
+    ps.stdout.close()
+    ps.wait()
+
+    output = str( output )
+
+    for line in output.split("\n"):
+        if line != None and proc in line : 
+            return 1
+        pass
+    pass
+
+    return 0
+pass
+
+if process_exists( "roscore" ) :
+	print( "roscore is running already!" )
+else :
+    print( "roscore is not running." )
+    if process_exists( "rosmaster" ) :
+        cmd = "killall -9 rosmaster"
+        print( cmd )
+        subprocess.call([ cmd ])
+    pass
+
+    cmd = "roscore &"
+    print( "running roscore" )
+    import os
+    os.system("roscore &")  
+pass
+
+print( "Good bye!" )
